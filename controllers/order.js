ca31e6098f1112635a88d41512e1e1a528a7cbd9
honglc <hongmainmail@gmail.com>
2023-02-27 17:12:36 +0700

    founded how to send mail to gmail by nodemailer, mailgen

--- controllers/order.js
+++ controllers/order.js
@@ -1,5 +1,51 @@
+const nodemailer = require("nodemailer");
+const mailgen = require("mailgen");
+
 const Order = require("../models/Order");
 const User = require("../models/User");
+
+const _sendMail = () => {
+  const config = {
+    service: "gmail",
+    auth: {
+      user: "hongmainmail@gmail.com",
+      pass: "fmxdsfegsrbigufu",
+    },
+  };
+  const tranport = nodemailer.createTransport(config);
+  const mailGenarator = new mailgen({
+    theme: "default",
+    product: { name: "Mailgen", link: "https://mailgen.js/" },
+  });
+  const response = {
+    body: {
+      name: "",
+      intro: "You buying something",
+      table: {
+        data: [
+          { item: "something", description: "some text", price: "some cash" },
+        ],
+      },
+      outro: "what is this, we dont know",
+    },
+  };
+
+  const mail = mailGenarator.generate(response);
+  const mess = {
+    from: "hongmainmail@gmail.com",
+    to: "changcan82@gmail.com",
+    subject: "Order",
+    html: mail,
+  };
+  tranport
+    .sendMail(mess)
+    .then(() => {
+      console.log("SENDED MAIL");
+    })
+    .catch((err) => {
+      console.log("err:", err);
+    });
+};
 
 exports.createOrder = async (req, res, next) => {
   try {
