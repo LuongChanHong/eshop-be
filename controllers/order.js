51d015911857618e7e11bd2aa9f7b47eaab948d6
honglc <hongmainmail@gmail.com>
2023-02-27 18:21:47 +0700

    add send gmail feature by nodemailer, mailgen

--- controllers/order.js
+++ controllers/order.js
@@ -4,41 +4,60 @@ const mailgen = require("mailgen");
 const Order = require("../models/Order");
 const User = require("../models/User");
 
-const _sendMail = () => {
-  const config = {
+const handleMailContent = (data, sendTime) => {
+  const _sendTime =
+    sendTime.getHours() +
+    ":" +
+    sendTime.getMinutes() +
+    " " +
+    sendTime.getDate() +
+    "/" +
+    parseInt(sendTime.getMonth() + 1) +
+    "/" +
+    sendTime.getFullYear();
+
+  let mailContent = {
+    body: {
+      name: `${data.user.fullname}`,
+      intro: `Order time: ${_sendTime}`,
+      table: {
+        data: data.products.map((item) => {
+          return {
+            name: item.productName,
+            price: item.price,
+            quantity: item.quantity,
+            subtotal: item.price * item.quantity,
+          };
+        }),
+      },
+      outro: `Total: ${data.totalPrice} VND`,
+    },
+  };
+  return mailContent;
+};
+
+const sendMail = async (data, sendTime) => {
+  const tranport = nodemailer.createTransport({
     service: "gmail",
     auth: {
       user: "hongmainmail@gmail.com",
       pass: "fmxdsfegsrbigufu",
     },
-  };
-  const tranport = nodemailer.createTransport(config);
+  });
+
   const mailGenarator = new mailgen({
     theme: "default",
-    product: { name: "Mailgen", link: "https://mailgen.js/" },
+    product: { name: "E SHOP", link: "https://mailgen.js/" },
   });
-  const response = {
-    body: {
-      name: "",
-      intro: "You buying something",
-      table: {
-        data: [
-          { item: "something", description: "some text", price: "some cash" },
-        ],
-      },
-      outro: "what is this, we dont know",
-    },
-  };
-
-  const mail = mailGenarator.generate(response);
-  const mess = {
-    from: "hongmainmail@gmail.com",
-    to: "changcan82@gmail.com",
-    subject: "Order",
-    html: mail,
-  };
+  const mailContent = handleMailContent(data, sendTime);
+  const mail = mailGenarator.generate(mailContent);
   tranport
-    .sendMail(mess)
+    .sendMail({
+      from: "hongmainmail@gmail.com",
+      to: data.user.email,
+      subject: "Order",
+      html: mail,
+    })
     .then(() => {
       console.log("SENDED MAIL");
     })
@@ -50,14 +69,14 @@ const _sendMail = () => {
 exports.createOrder = async (req, res, next) => {
   try {
     const reqData = req.body;
-    // console.log("reqData:", reqData);
-    await User.findByIdAndUpdate(reqData.userId, { address: reqData.address });
-
+    // console.log("reqData.user:", reqData.user);
+    await User.findByIdAndUpdate(reqData.user.userId, reqData.user);
     const newOrder = new Order({
       user: reqData.user,
       products: reqData.products,
       totalPrice: reqData.totalPrice,
     });
+    await sendMail(req.body, newOrder.orderTime);
     newOrder.save();
   } catch (error) {
     return next(new Error(error));
