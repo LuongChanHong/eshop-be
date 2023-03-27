8814ae90db918da2ab6b6a4a35f217358d882fa0
honglc <hongmainmail@gmail.com>
2023-03-27 11:38:17 +0700

    log middleWare auth error

--- customMiddleware/auth.js
+++ customMiddleware/auth.js
@@ -5,6 +5,7 @@ exports.requiresLogin = (req, res, next) => {
   // console.log("======================");
 
   const reqCookie = req.headers.cookie;
+  console.log("reqCookie:", reqCookie);
   const userId = reqCookie.split(";")[1].split("=")[1];
   const token = reqCookie.split(";")[0].split("=")[1];
 
